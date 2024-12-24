f9294da72f5978a5800305d0fa2534750b8fe1ef
Nguyễn Minh Tuân <minhtuanng12@gmail.com>
2024-12-24 17:37:17 +0700

    deploy

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,108 @@
-import Image from "next/image";
+"use client";
+
+import { useState, useEffect } from "react";
+import Snowfall from "./components/Snowfall";
+import Envelope from "./components/Envelop";
+import Content from "./components/Content";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    const [isOpen, setIsOpen] = useState(false);
+    const [password, setPassword] = useState("");
+    const [ig, setIg] = useState("@");
+    const correctPassword = "21082002";
+    const [error, setError] = useState("");
+    const [correct, setCorrect] = useState(false);
+    const [openInput, setOpenInput] = useState(true);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (
+            password === correctPassword &&
+            (ig === "@_babe.na_" || ig === "@babe.na")
+        ) {
+            setCorrect(true);
+            setOpenInput(false);
+            setTimeout(() => {
+              setIsOpen(true);
+            }, 500)
+        } else {
+            if (password !== correctPassword) {
+                setError("Ngày sinh của mình mà cũng không nhớ?");
+            }
+        }
+    };
+
+    useEffect(() => {
+        document.body.style.overflow = "hidden";
+        return () => {
+            document.body.style.overflow = "visible";
+        };
+    }, []);
+
+    return (
+        <div className="w-screen h-screen items-center justify-center relative">
+            <img
+                src={"./theme.jpg"}
+                className="absolute min-h-auto min-w-screen flex items-center justify-center overflow-hidden"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div className=" w-full h-full">
+                <Snowfall />
+                <div className="w-full max-w-md z-10 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]">
+                    {openInput ? (
+                        <form
+                            onSubmit={handleSubmit}
+                            className="mb-4 text-center"
+                        >
+                            <h1 className="text-4xl font-bold text-white mb-6 font-serif">
+                                {error ? (
+                                    <div className="text-[yellow] text-[20px]">
+                                        {error}
+                                    </div>
+                                ) : (
+                                    `Tun's Secret Message`
+                                )}
+                            </h1>
+                            <input
+                                type="text"
+                                value={ig}
+                                onChange={(e) => {
+                                    if (e.target.value) {
+                                        setError("");
+                                        setIg(e.target.value);
+                                    }
+                                }}
+                                placeholder="@"
+                                className="w-full p-3 mb-4 border-2 border-gold rounded-full text-center text-lg bg-transparent text-white placeholder-gold"
+                            />
+                            <input
+                                type="password"
+                                value={password}
+                                onChange={(e) => {
+                                    setError("");
+                                    setPassword(e.target.value);
+                                }}
+                                placeholder={`${
+                                    ig === "@" ? "" : ig
+                                }'s birthday`}
+                                className="w-full p-3 mb-4 border-2 border-gold rounded-full text-center text-lg bg-transparent text-white placeholder-gold"
+                            />
+                            <button
+                                type="submit"
+                                className="w-full bg-gold text-blue-900 p-3 rounded-full text-lg font-bold hover:bg-yellow-400 transition-colors"
+                            >
+                                Gooooo
+                            </button>
+                        </form>
+                    ) : null}
+                    {correct ? (
+                        <Envelope isOpen={isOpen}>
+                            <Content />
+                        </Envelope>
+                    ) : (
+                        ""
+                    )}
+                </div>
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
